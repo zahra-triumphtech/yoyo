33d57977e50b5cd1ecd8f98e9c817707bf0d1c28
Zahra Tariq <zahra@triumphtech.com>
2023-05-30 19:19:50 +0500

    latest code

--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -1,11 +1,11 @@
 module "egress_vpc" {
-  source                = "../modules/egress-vpc"
+  source                = "../modules/egressvpc"
   type                  = "egress"
-  egress_vpc_configs    = var.egress_vpc_configs
+  vpc_configs           = var.egress_vpc_configs
   flowlog_retention     = var.egress_flowlog_retention
   flowlog_traffic_type  = var.egress_flowlog_traffic_type
-  egress_public_subnet_configs = var.egress_public_subnet_configs
-  egress_tgw_subnet_configs    = var.egress_tgw_subnet_configs
+  public_subnet_configs = var.egress_public_subnet_configs
+  tgw_subnet_configs    = var.egress_tgw_subnet_configs
   tags                  = var.tags
 }
 
@@ -31,16 +31,16 @@ module "ingress_vpc" {
   tags                   = var.tags
 }
 module "inspection_vpc" {
-  source                        = "../modules/inspection-vpc"
-  type                          = "inspection"
-  inspection_vpc_configs                   = var.inspection_vpc_configs
-  flowlog_retention             = var.inspection_flowlog_retention
-  flowlog_traffic_type          = var.inspection_flowlog_traffic_type
-  inspection_mgmt_subnet_configs = var.inspection_mgmt_subnet_configs
-  inspection_public_subnet_configs = var.inspection_public_subnet_configs
-  inspection_fw_subnet_configs = var.inspection_fw_subnet_configs
-  inspection_gwlb_subnet_configs = var.inspection_gwlb_subnet_configs
-  inspection_gwlbe_subnet_configs = var.inspection_gwlbe_subnet_configs
-  inspection_tgw_subnet_configs = var.inspection_tgw_subnet_configs
-  tags                          = var.tags
+  source                 = "../modules/inspection-vpc"
+  type                   = "inspection"
+  inspection_vpc_configs = var.inspection_vpc_configs
+  flowlog_retention      = var.inspection_flowlog_retention
+  flowlog_traffic_type   = var.inspection_flowlog_traffic_type
+  mgmt_subnet_configs    = var.inspection_mgmt_subnet_configs
+  public_subnet_configs  = var.inspection_public_subnet_configs
+  fw_subnet_configs      = var.inspection_fw_subnet_configs
+  gwlb_subnet_configs    = var.inspection_gwlb_subnet_configs
+  gwlbe_subnet_configs   = var.inspection_gwlbe_subnet_configs
+  tgw_subnet_configs     = var.inspection_tgw_subnet_configs
+  tags                   = var.tags
 }
